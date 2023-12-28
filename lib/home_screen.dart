ae27e0649660e90722b38349ea3957fe016176ec
dohuns <gkgk586@gmail.com>
2023-12-28 22:03:29 +0900

    Timer 객체를 사용하여 카운트

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -10,36 +10,35 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int totalSeconds = 1500;
+  bool isRunning = false;
+  late Timer timer;
+
+  void onTick(Timer timer) {
+    setState(() {
+      totalSeconds -= 1;
+    });
+  }
+
+  // 시작 버튼 클릭 이벤트
+  void onStartPressed() {
+    setState(() {
+      isRunning = true;
+    });
+    timer = Timer.periodic(const Duration(seconds: 1), onTick);
+  }
+
+  // 정지 버튼 클릭 이벤트
+  void onPausePressed() {
+    timer.cancel();
+    setState(() {
+      isRunning = false;
+      totalSeconds = 1500;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    int totalSeconds = 1500;
-    bool isRunning = false;
-    late Timer timer;
-
-    void onTick(Timer timer) {
-      setState(() {
-        totalSeconds -= 1;
-      });
-    }
-
-    // 시작 버튼 클릭 이벤트
-    void onStartPressed() {
-      print('onStartPressed $isRunning');
-      setState(() {
-        isRunning = true;
-      });
-      timer = Timer.periodic(const Duration(seconds: 1), onTick);
-    }
-
-    // 정지 버튼 클릭 이벤트
-    void onPausePressed() {
-      print('onStopPressed');
-      timer.cancel();
-      setState(() {
-        isRunning = false;
-      });
-    }
-
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
       body: Column(
